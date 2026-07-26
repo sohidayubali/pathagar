4977cbbc75315aefac6a9785d53415ee586b3592
sohidayubali <ayubpathagar@gmail.com>
2026-07-26 14:00:45 +0600

    Create config.js

---
+++ js/js/config.js
@@ -0,0 +1,3 @@
+// Google Apps Script ডিপ্লয় করার পর যে Web App URL পাবেন সেটা এখানে বসান
+// উদাহরণ: https://script.google.com/macros/s/AKfycb.../exec
+const API_URL = "https://script.google.com/macros/s/AKfycbzSN11k18NnAy0gd_srWByEPfvqvTIFWv2MHehuMdHcmliA2FBGvxqw110bLNR2PDsxOg/exec";
